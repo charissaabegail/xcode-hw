aa95b3e9e642ffe7a414f5e33d1b1f2f6a4fbc67
charissaabegail <morales@mail.hs-ulm.de>
2020-10-02 17:03:21 +0200

    Initial Commit

---
+++ hello-world/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hello-world
+//
+//  Created by Charissa Abegail M. Morales on 10/2/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
